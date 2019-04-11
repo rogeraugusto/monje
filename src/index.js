6ae8bb65d0086fc066b8690efc64a03892b31562
Roger Moraes <18172346+rogeraugusto@users.noreply.github.com>
2019-04-11 19:20:59 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-/* configurar o middleware express.static */
 app.use(express.static('./src/public'));
 
-// Inclusão dos Middlewares do projeto, que adiciona features para tratar os requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
@@ -27,12 +25,6 @@ require('./controllers/index.js')(app);
 require('./controllers/auth.js')(app);
 require('./controllers/device.js')(app);
 require('./controllers/done.js')(app);
-
-/*
-app.all('*', function(req, res) {
-  	res.redirect('/login')
-});
-*/
 
 app.listen(3000, () => {
     console.log('Server ok')
