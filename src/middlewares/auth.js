4b8d491d08b46d0fadcb0050bfcc015a71558ec9
Roger Moraes <18172346+rogeraugusto@users.noreply.github.com>
2019-04-11 19:21:30 -0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,15 +4,12 @@ const authConfig = require('../config/auth');
 module.exports = (req, res, next) => {
 
 	let sess = req.session;
-	const authHeader = sess.token; //req.headers.authorization;
+	const authHeader = sess.token; 
 
 	if (!authHeader) {
 		res.render('login', { authError: 'No token provided' });
 		return;
 	}
-
-	// Quebra o token pra identificar se existe o início dele, Bearer
-	// Testa se tem duas partes
 
 	const parts = authHeader.split(' ');
 
@@ -31,8 +28,6 @@ module.exports = (req, res, next) => {
 	jwt.verify(token, authConfig.secret, (err, decoded) => {
 		if (err) res.render('login', { authError: 'Token invalid' });
 
-		// Após todas as validações no token, passa o id do usuário que foi usado pra gerar o token
-		// devolta pra aplicação, pra ser usado em qualquer controller
 		req.userId = decoded.id;
 		return next();
 	});
